58f0aa1508192af0000070119cc8fccc544d8ab5
L F <franco100lucas@gmail.com>
2023-10-19 22:59:17 -0300

    Adicionando códigos comentados para estudo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
+
+const Hello = (props) => { // props = propriedades
+  console.log(props)
+  return(
+    <div>
+      <p>Olá {props.name}, você tem {props.age} anos</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = "Nome"
+  const age = "10"
+  return(
+    // <div>
+    <>  // Um exemplo de como empacotar vários elementos sem precisar de uma div
+      <style>
+        {`
+          body {
+            background-color: blue;
+            font-size: 30px;
+          }
+        `}
+      </style>
+      <h1>Olá a todos"</h1>
+      <Hello name="Lucas" age={20 + 2}/>
+      <Hello name={name} age={age}/>
+    {/* </div> */}
+    </> //Tag de fechamento do empacotamento
+  )
+}
+
 // const App = () => {
 //   const hoje = new Date()
 //   const a = 10
@@ -14,25 +46,29 @@
 //   )
 // }
 
-const Hello = (props) => { // props = propriedades
-  console.log(props)
-  return(
-    <div>
-      <p>Olá {props.name}, você tem {props.age} anos</p>
-    </div>
-  )
-}
+// const Friends = () => {
+//   const amigos = [ 
+//       { nome: 'Peter', idade: 4 },
+//       { nome: 'Maya', idade: 10 },
+//     ]
 
-const App = () => {
-  const name = "Nome"
-  const age = "10"
-  return(
-    <div>
-      <h1>Olá a todos"</h1>
-      <Hello name="Lucas" age={20 + 2}/>
-      <Hello name={name} age={age}/>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <p>{amigos[0].nome} {amigos[0].idade}</p> //React não aceita tipo Object,
+//      apenas no caso de utilizar o .map() para percorrer o vetor
+//       <p>{amigos[1].nome} {amigos[1].idade}</p>
+//     </div>
+//   )
+// }
+
+// const FreindsVetor = () => {
+//   const amigos = [ 'Peter', 'Maya']
+
+//   return (
+//     <div>
+//       <p>{amigos}</p>
+//     </div>
+//   )
+// }
 
 export default App
