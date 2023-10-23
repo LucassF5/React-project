b23c07785e4a6317cc5b365fabadfb2f2e9cd047
L F <franco100lucas@gmail.com>
2023-10-22 22:47:14 -0300

    Definindo botões que alteram o estado do contador

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
-const Hello = (props) => { // props = propriedades
-  console.log(props)
-  return(
-    <div>
-      <p>Olá {props.name}, você tem {props.age} anos</p>
-    </div>
-  )
-}
+import { useState } from 'react'
+
+// const ButtonAcrescentar = () => {
+
+//   const [ contador, setContador ] = useState(0)
+
+//   return(
+//     <div>
+//       <div>{contador}</div>
+//       <button onClick={() => setContador(contador + 1)}>
+//         ACRESCENTAR
+//       </button>
+//     </div>
+//   )
+// }
+
+// const ButtonZerar = () => {
+
+//   const [ contador, setContador ] = useState(0)
+  
+//   return(
+//     <div>
+//       <button onClick={() => setContador(0)}> 
+//         zerar
+//       </button>
+//     </div>
+//   )
+// }
 
 const App = () => {
-  const name = "Nome"
-  const age = "10"
-  return(
-    // <div>
-    <>  
-    {/* Um exemplo de como empacotar vários elementos sem precisar de uma div */}
-      <style>
-        {`
-          body {
-            background-color: blue;
-            font-size: 30px;
-          }
-        `}
-      </style>
-      <h1>Olá a todos"</h1>
-      <Hello name="Lucas" age={20 + 2}/>
-      <Hello name={name} age={age}/>
-    {/* </div> */}
-    </> //Tag de fechamento do empacotamento
+  const [ contador, setContador ] = useState(0)
+
+  console.log('renderizando com o valor do contador em', contador)
+
+  const aumentarEmUm = () => {
+
+    console.log('aumentando, valor anterior', contador)
+    setContador(contador + 1)
+  }
+
+  const diminuirEmUm = () => { 
+
+    console.log('diminuindo, valor anterior', contador)
+    setContador(contador - 1)
+  }
+
+  const zerarContador = () => {
+
+    console.log('zerando, valor anterior', contador)
+    setContador(0)
+  }
+
+  return (
+    <div>
+      <Exibir contador={contador} />
+      <Botao onClick={aumentarEmUm} texto="mais+" />
+      <Botao onClick={zerarContador} texto="zerar" />
+      <Botao onClick={diminuirEmUm} texto="menos-" />
+    </div>
   )
-}
+} 
+
+const Exibir = ({ contador }) => <div><h1>{contador}</h1></div>
+
+const Botao = ({ onClick, texto }) => <button onClick={onClick}>{texto}</button>
 
 export default App
