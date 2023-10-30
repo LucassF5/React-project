73bc484a574d95709b29438dcc408af8cfdf0814
L F <franco100lucas@gmail.com>
2023-10-30 15:21:46 -0300

    componente contador de botões inicia com clique

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
+
+const Historico = (props) => {
+  if (props.todosOsCliques.length === 0) {
+    return (
+      <div>
+        Clique em um dos botões para usar a aplicação!
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      Histórico de cliques nos botões: {props.todosOsCliques.join(' ')}
+    </div>
+  )
+}
+
+const Botao = (props) => { 
+  console.log(props)
+  const { handleClique, texto } = props
+  return (
+    <button onClick={handleClique}>
+      {texto}
+    </button>
+  )
+}
+
+// const Botao = ({ handleClique, texto }) => (
+//   <button onClick={handleClique}>
+//     {texto}
+//   </button>
+// )
 
 const App = () => {
   const [esquerda, setEsquerda] = useState(0)
   const [direita, setDireita] = useState(0)
-
   const [todosOsCliques, setTodos] = useState([])
 
-  const [total, setTotal] = useState(0)
-
-  // useEffect(() => {
-  //   alert('Qualquer coisa') //Vai aparecer no console do navegador
-  // }, [esquerda, direita]) //usar [] está indicando que vai aparecer na mudança de estado do que está dentro do array
-  // //Vai aparecer quando o valor de esquerda ou direita mudar
-
-
   const handleCliqueEsquerda = () => {
-    setTodos(todosOsCliques.concat('E')) //Vai adicionar o valor E no array quando for clicado 
-    const atualizaEsquerda = esquerda + 1
-    setEsquerda(atualizaEsquerda)
-    setTotal(atualizaEsquerda + direita)
-  } 
-
+    console.log('Clique esquerda')
+    setTodos(todosOsCliques.concat('E'))
+    setEsquerda(esquerda + 1)
+  }
 
   const handleCliqueDireita = () => {
-    setTodos(todosOsCliques.concat('D')) //Vai adicionar o valor D no array quando for clicado
-    const atualizaDireita = direita + 1
-    setDireita(atualizaDireita)
-    setTotal(atualizaDireita + esquerda)
+    console.log('Clique direita')
+    setTodos(todosOsCliques.concat('D'))
+    setDireita(direita + 1)
   }
 
   return (
     <div>
       {esquerda}
-      <button onClick={handleCliqueEsquerda}>Esquerda</button>
-      <button onClick={handleCliqueDireita}>Direita</button>
+      <Botao handleClique={handleCliqueEsquerda} texto='Esquerda' />
+      <Botao handleClique={handleCliqueDireita} texto='Direita' />
       {direita}
-
-      <p>{todosOsCliques.join(' ')}</p> 
-
-      <p>Total {total}</p>
-      {/* Vai juntar os valores do array e colocar um espaço entre eles
-      Vai mostrar todos os cliques que foram feitos/mostrar os valores do array */}
+      <Historico todosOsCliques={todosOsCliques} />
     </div>
   )
 }
