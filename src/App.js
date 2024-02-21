3573f15935e125ea00d18a09b8749e013cfe1278
L F <franco100lucas@gmail.com>
2024-02-21 00:51:24 -0300

    Finalizando implementação do sistema de adição de notas e servidor

--- src/App.js
+++ src/App.js
@@ -1,51 +1,40 @@
-import "./App.css";
-import Note from "./components/Note"
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
+import noteService from './services/notes'
+import './App.css'
 
-const App = (props) => {
-  const [notes, setNotes] = useState([]) 
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
-  const hook = () => {
-    // Hook vai ser a constante que ativa o efeito
-    console.log('effect (efeito)') // imprime quando o componente é renderizado
-    axios.get('http://localhost:3001/notes').then((response) => {
-    // Tendo sucesso, a função de retorno de chamada 
-    //é chamada com a resposta do servidor como seu argumento.
-      console.log('promise fulfilled (promessa resolvida)')
-      setNotes(response.data)
-      // A função setNotes atualiza o estado de notes com a resposta do servidor
-    })
-  }
-  
-  useEffect(hook, [])
-
-  console.log('render (renderiza)', notes.length, 'notes (notas)')
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+      important: Math.random() > 0.5,
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response=>{   
-        setNotes(notes.concat(response.data ))
-        setNewNote("")
-          // console.log(response)
+    noteService
+      .create(noteObject)
+        .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
       })
-
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
   }
 
-  const handleNoteChange = (event) => { 
-    console.log(event.target.value)
+  const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
 
@@ -53,28 +42,50 @@ const App = (props) => {
     ? notes
     : notes.filter(note => note.important)
 
+   const toggleImportanceOf = id => {
+      const note = notes.find(n => n.id === id)
+      const changedNote = { ...note, important: !note.important }
+  
+      noteService
+        .update(id, changedNote).then(returnedNote => {
+          setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+        })
+        .catch(error => {
+          setErrorMessage(
+            `Note '${note.content}' was already removed from server`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+          setNotes(notes.filter(n => n.id !== id))
+        })
+    }
+
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
-        <button onClick={() => setShowAll(!showAll)}> 
-        //alterna o valor de showAll de verdadeiro para falso
+        <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
-            
         </button>
-      </div>
+      </div> 
       <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
-        )}
+        <ul>
+          {notesToShow.map(note => 
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          )}
+        </ul>
       </ul>
       <form onSubmit={addNote}>
-      <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form>   
+      </form>
+      <Footer />
     </div>
   )
 }
